0be2771d252a586e5ff39fc13a09f6ff250e5fa2
Aarti Gundgole <gundgoleaarti@gmail.com>
2026-06-03 09:52:30 +0530

    Intial Playwright Project Setup

---
+++ tests/UIBasics.spec.js
@@ -0,0 +1,18 @@
+const {test, expect} = require ('@playwright/test')
+
+test('Open Browser', async ({browser})=>
+{
+
+const context = await browser.newContext();
+const page = await context.newPage();
+await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+
+console.log(await page.title());
+
+await expect(page).toHaveTitle("LoginPage Practise | Rahul Shetty Academy");
+
+await page.locator("#username").fill("rahulshettyacademy");
+await page.locator("[type='password']").fill("Learning@830$3mK2");
+await page.locator("#terms").click();
+await page.locator("#signInBtn").click();
+});
